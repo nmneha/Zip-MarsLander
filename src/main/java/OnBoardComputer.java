c48acac7dcf1040e8829ba76073d4d6a2939c138
nusera <nusera.neha@gmail.com>
2022-03-25 23:37:12 -0400

    landed safely

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -2,8 +2,11 @@ public class OnBoardComputer implements BurnStream {
 
     @Override
     public int getNextBurn(DescentEvent status) {
-        int burn = 0;
-        
+        int v = status.getVelocity();
+        int a = status.getAltitude();
+        int burn = ((v*v)/(2*a)) +99;
+
+
         System.out.println(burn); /*hack!*/
         return burn;
     }
--- src/test/java/SimulationTest.java
+++ src/test/java/SimulationTest.java
@@ -31,7 +31,7 @@ public class SimulationTest {
         BurnStream burnSource = new OnBoardComputer();
         Simulation game = new Simulation(new Vehicle(10000));
         int okay = game.runSimulation(burnSource);
-        Assert.assertEquals(okay, Vehicle.SUCCESS);
+        Assert.assertEquals(Vehicle.SUCCESS, okay);
     }
 
     @Test
@@ -40,7 +40,7 @@ public class SimulationTest {
         Simulation game = new Simulation(new Vehicle(Simulation.randomaltitude()));
         //Simulation game = new Simulation(new Vehicle(15000));
         int okay = game.runSimulation(burnSource);
-        Assert.assertEquals(okay, Vehicle.SUCCESS);
+        Assert.assertEquals(Vehicle.SUCCESS, okay);
     }
 
 }
